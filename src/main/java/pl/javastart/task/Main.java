3cdaf8d1b7f051a57001d1d211f8d2d5d4e2d95d
Konrad Putelbergier <Konrad.Putelbergier@gmail.com>
2023-08-18 14:51:01 +0200

    Zadanie 10.1

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,8 +1,44 @@
 package pl.javastart.task;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Book[] books = new Book[3];
+        int counter = 0;
 
+        System.out.println("Podaj informację o trzech unikalnych książkach");
+
+        while (counter < books.length) {
+            System.out.println("Podaj tytuł nowej ksiazki");
+            String title = scan.nextLine();
+            System.out.println("Podaj liczbę stron nowej ksiazki");
+            int pages = scan.nextInt();
+            scan.nextLine();
+            Book book = new Book(title, pages);
+            boolean isDuplicate = false;
+            for (int i = 0; i < counter; i++) {
+                if (books[i].equals(book)) {
+                    isDuplicate = true;
+                    System.out.println("Duplikat");
+                    break;
+                }
+            }
+            if (!isDuplicate) {
+                books[counter] = book;
+                counter++;
+            }
+        }
+        System.out.println("\nObiekty zapisane w tablicy:");
+
+        for (Book book : books) {
+            if (book != null) {
+                System.out.println(book);
+            }
+        }
     }
 }
+
+
